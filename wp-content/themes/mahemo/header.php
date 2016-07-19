e3c838ef7f3d1f3d926e1c37b384fcc4ef566984
Matheus Henrique <matheus.henri@me.com>
2016-07-18 22:05:53 -0300

    Atualizção Home

--- wp-content/themes/mahemo/header.php
+++ wp-content/themes/mahemo/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title><?php if( is_front_page() ) { bloginfo('name'); } else {} ; ?></title>
+    <title><?php if( is_front_page() ) { bloginfo('name'); } else { echo the_title(); } ; ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -16,27 +16,31 @@
     <div class="inner">
         <div class="col-12">
             <div class="breadcrumb">
-                <?php echo esc_attr__( bloginfo( 'name' ) ); ?>
+                <?php echo esc_attr__( bloginfo( 'name' ) . ' > ' . the_title() ); ?>
             </div>            
         </div>
         <?php if( is_front_page() ) : ?>
             <header id="frontHeader">
-                <div class="container">
-                    <div>
-                        <a href="#" class="showNav"><i></i></a>
-                    </div>
-                    <div>
-                        <a href="<?php echo site_url(); ?>">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-mahemo.svg" alt="Logo Mahemo">
-                        </a>
-                    </div>
-                    <div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/mouse-scroll.svg" alt="Scroll Down">
+                <div class="innerHeader">
+                    <div class="container">
+                        <div>
+                            <a href="#" class="showNav"><i></i></a>
+                        </div>
+                        <div>
+                            <a href="<?php echo site_url(); ?>">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-mahemo.svg" alt="Logo Mahemo">
+                            </a>
+                        </div>
+                        <div>
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/mouse-scroll.svg" alt="Scroll Down">
+                        </div>
                     </div>
                 </div>
             </header>
         <?php else : ?>
-            <h1>Não é front-page</h1>
+            <header id="mainHeader">
+                <h1><?php the_title(); ?></h1>                
+            </header>
         <?php endif; ?>
        
       
